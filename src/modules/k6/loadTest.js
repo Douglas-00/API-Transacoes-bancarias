3d930fec2210551ed26e17566de39c543f7128a8
Douglas-00 <alvesdouglas547@gmail.com>
2024-10-30 19:32:23 -0300

    cenários teste de carga api

---
+++ src/modules/k6/loadTest.js
@@ -0,0 +1,34 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '5s', target: 50 },
+    { duration: '5s', target: 100 },
+    { duration: '3s', target: 100 },
+    { duration: '5', target: 0 },
+  ],
+};
+
+export default function () {
+  let payload = JSON.stringify({
+    accounts: [],
+    transactions: [
+      { accountId: 1, type: 'DEPOSIT', amount: 100 },
+      { accountId: 1, type: 'WITHDRAW', amount: 50 },
+      { accountId: 1, type: 'TRANSFER', amount: 30, destinyId: 2 },
+      { accountId: 1, type: 'DEPOSIT', amount: 50 },
+      { accountId: 1, type: 'TRANSFER', amount: 20, destinyId: 2 },
+    ],
+  });
+
+  let res = http.post('http://localhost:3000/account', payload, {
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  check(res, {
+    'status is 201': (r) => r.status === 201,
+  });
+
+  sleep(1);
+}
