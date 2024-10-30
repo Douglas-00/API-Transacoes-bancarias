72d5b906c92e279638a769c47f7ff4bf11e453fd
Douglas-00 <alvesdouglas547@gmail.com>
2024-10-30 10:28:59 -0300

    chore: importando modulos de conta e transação para o app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { LoggerModule } from './modules/logger/logger.module';
+import { AccountModule } from './modules/account/account.module';
+import { TransactionModule } from './modules/transaction/transaction.module';
 
 @Module({
-  imports: [PrismaModule, LoggerModule],
+  imports: [PrismaModule, LoggerModule, AccountModule, TransactionModule],
   controllers: [],
   providers: [],
 })
